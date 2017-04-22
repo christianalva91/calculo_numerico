e8bcb668ae3e236e8ef5da1fb6341ca63e3e1cd6
Christian alvarado <Christian alvarado>
2017-04-22 12:00:10 -0500

    cambio rk4

--- mRK4.m
+++ mRK4.m
@@ -6,28 +6,22 @@ w(1) = y0;
 t(1) = I(1);
 h = (I(2) - I(1))/n;
 
-
 for i = 1:n
     t(i+1) = t(i) + h;
-    S1 = feval(f,t(i),w(i));
-    S2 = feval(f,t(i)+(h/2),w(i) + (h/2)*S1);
-    S3 = feval(f,t(i)+(h/2),w(i) + (h/2)*S2);
-    S4 = feval(f,t(i)+h,w(i) + h*S3);
-    z = S1 + 2*S2 + 2*S3 + S4;
-    w(i+1) = w(i) + ((h/6)*z);
+    w(i+1) = w(i) + ((h/6)*RK(f, t(i),w(i)));
 end
 
 Tn = t;
 Wn = w;
 
-%     function z = RK(t ,w)
-%     S1 = feval(f,t,w);
-%     S2 = feval(f,t+(h/2),w + (h/2)*S1);
-%     S3 = feval(f,t+(h/2),w + (h/2)*S2);
-%     S4 = feval(f,t+h,w + h*S3);
-% 
-%     z = S1 + 2*S2 + 2*S3 + S4;
-%     end
+function [w] = RK(f, t ,w)
+        S1 = feval(f,t,w);
+        S2 = feval(f,t+(h/2),w + (h/2)*S1);
+        S3 = feval(f,t+(h/2),w + (h/2)*S2);
+        S4 = feval(f,t+h,w + h*S3);
+
+        w = S1 + 2*S2 + 2*S3 + S4;
+end
 
 
 end
