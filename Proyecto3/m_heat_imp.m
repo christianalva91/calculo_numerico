bbd8ab584dd4b9dfabd6ce94b4a93c764c1b2e5f
unknown <lvillalpg@CC131.itam.mx>
2017-05-08 18:31:14 -0500

    creación métodos

---
+++ Proyecto3/m_heat_imp.m
@@ -0,0 +1,30 @@
+function W = m_heat_imp(Ix, It, M, N)
+% In : Ix ... space interval [a, b]
+% It ... time interval [t0, t0 + T]
+% M ... number of space steps
+% N ... number of time steps
+%
+% Out: W ... numerical solution
+f=@(x) cos(pi*x);
+l=@(t) exp(-pi*t);
+r=@(t) -exp(-pi*t);
+D=1/pi;
+
+xl=Ix(1); xr=Ix(2); yb=It(1); yt=It(2);
+h=(xr-xl)/M; k=(yt-yb)/N; m=M-1; n=N;
+sigma=D*k/(h*h);
+
+a=diag(1+2*sigma*ones(m,1))+diag(-sigma*ones(m-1,1),1);
+a=a+diag(-sigma*ones(m-1,1),-1); % define matrix a
+
+lside=l(yb+(0:n)*k); rside=r(yb+(0:n)*k);
+W(:,1)=f(xl+(1:m)*h)'; % initial conditions
+
+for j=1:n
+W(:,j+1)=a\(W(:,j)+sigma*[lside(j);zeros(m-2,1);rside(j)]);
+end
+
+W=[lside;W;rside]; % attach boundary conds
+
+end
+
